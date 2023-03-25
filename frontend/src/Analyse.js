94d6c3d039b3f539d0138e70af37075cccca9037
Pranav Ambiga <pranav.ambig123@gmail.com>
2023-03-25 21:29:08 +0530

    minor stuff-2

--- frontend/src/Analyse.js
+++ frontend/src/Analyse.js
@@ -2,6 +2,8 @@ import { Line } from 'react-chartjs-2';
 import { useState } from 'react';
 import NavBar from './NavBar';
 import './Analyse.css'
+import axios from 'axios';
+
 import {
 	Chart as ChartJS,
 	CategoryScale,
@@ -31,18 +33,38 @@ export const options = {
 export default function Analyse(){
 
 	const [tensionval, setTensionVal] = useState(0.5)
+	const [prodname, setProdName] = useState("")
+	const [tillDate, setTillDate] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+	const [predicted, setPredicted] = useState([13, 14, 15, 16])
+
+	let secondGraphValues = []
+	const updateSGV = ()=>{
+		for (let i=0; i<tillDate.length-1; i++){
+			secondGraphValues.push(null)
+		}
+		secondGraphValues.push(tillDate[tillDate.length-1])
+		for (let i=0; i<predicted.length; i++){
+			secondGraphValues.push(predicted[i])
+		}
+		// console.log(secondGraphValues)
+	}
+	updateSGV() //second graph values
+
+	
 
 	let data = {
-		labels: [1, 2, 3, 4, 5, 6, 7, 8],
+		labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
 		datasets: [{label:'Actual',
-					data:[1, 2, 3, 4, 2, 6, 7, 8],
+					data:tillDate,
 					borderColor: "rgb(142, 126, 164)",
+					backgroundColor: "rgb(142, 126, 164)",
 					tension: tensionval,
 					// fill:true
 					},
 					{label:'Predicted',
-					data:[1, 2, 3, 4, 2, 6, 5, 5],
+					data: secondGraphValues,
 					borderColor: "rgb(255, 157, 0)",
+					backgroundColor: "rgb(255, 157, 0)",
 					tension: tensionval,
 					// fill:true
 					},
@@ -51,26 +73,43 @@ export default function Analyse(){
 
 	const rangeHandler = ()=>{
 		let ele = document.getElementById('tensionval')
-		console.log(ele.value/100)
+		// console.log(ele.value/100)
 		setTensionVal(ele.value/100)
 	}
 
+	const reqFromBackend = (e)=>{
+		if (e.which == 13){
+			let ele = document.getElementById('product-input')
+			axios.post('http://127.0.0.1:5000/prod', {
+				"PLID": ele.value
+			  })
+			  .then(function (response) {
+				if (response["data"] != "Error"){
+					setProdName(response["data"][0])
+					setTillDate(response["data"][1])
+					setPredicted(response["data"][2])
+					updateSGV()
+				}
+			  })
+		}
+	}
+
 	return(
-		<div>
+		<div id='maiin-ctn'>
 			<NavBar></NavBar>
 			<div id='main-ctn'>
 
 				<div id='leftside'>
 					<p id='left-title'>Product</p>
-					<input id='product-input' type='text' placeholder='Search for a Product'/>
+					<input id='product-input' type='text' placeholder='Search for a Product' onKeyDown={reqFromBackend}/>
 					
 					<div id='controls'>
 						<div className='control-pair'>
 							<p className='param'>Product Name:</p>
-							<p className='pvalue'>Laptop</p>
+							<p className='pvalue'>{prodname}</p>
 						</div>
 						<div className='control-pair'>
-							<p className='param'>Smoothness:</p>
+							<p className='param'>Smoothing:</p>
 							<input className='pvalue' id='tensionval' type={'range'} onChange={rangeHandler} />
 						</div>
 					</div>
